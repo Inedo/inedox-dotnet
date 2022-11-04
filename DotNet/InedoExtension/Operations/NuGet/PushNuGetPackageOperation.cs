6d0eb5e9a720351418d0e2dafa1cc0ce97e9c09e
Greg Divis <gdivis@inedo.com>
2022-11-04 15:45:45 -0400

    fix nuget push

--- DotNet/InedoExtension/Operations/NuGet/PushNuGetPackageOperation.cs
+++ DotNet/InedoExtension/Operations/NuGet/PushNuGetPackageOperation.cs
@@ -1,16 +1,17 @@
 ﻿using System.ComponentModel;
-using System.Threading.Tasks;
 using Inedo.Agents;
 using Inedo.Diagnostics;
 using Inedo.Documentation;
+using Inedo.ExecutionEngine.Executer;
 using Inedo.Extensibility;
 using Inedo.Extensibility.Credentials;
 using Inedo.Extensibility.Operations;
-using Inedo.Extensibility.SecureResources;
 using Inedo.Extensions.Credentials;
+using Inedo.Extensions.PackageSources;
 using Inedo.Extensions.SecureResources;
 using Inedo.Web;
-using UsernamePasswordCredentials = Inedo.Extensions.Credentials.UsernamePasswordCredentials;
+
+#nullable enable
 
 namespace Inedo.Extensions.DotNet.Operations.NuGet
 {
@@ -18,46 +19,46 @@ namespace Inedo.Extensions.DotNet.Operations.NuGet
     [ScriptAlias("Push-Package")]
     [DisplayName("Push NuGet Package")]
     [Description("Publishes a NuGet package file to a NuGet package source.")]
-    [Note("When Username/Password are specified, it will be passed as \"Username:Password\" via the API Key.")]
     [DefaultProperty(nameof(PackagePath))]
     public sealed class PushNuGetPackageOperation : NuGetOperation
     {
         [Required]
         [ScriptAlias("FilePath")]
         [ScriptAlias("Package")]
-        [DisplayName("Path to .nupkg package file name")]
-        public string PackagePath { get; set; }
+        [DisplayName("Package file name")]
+        [PlaceholderText("example: myPackage-1.0.0.nupkg")]
+        public string? PackagePath { get; set; }
         [ScriptAlias("To")]
         [ScriptAlias("Source")]
         [DisplayName("Package source")]
-        [SuggestableValue(typeof(SecureResourceSuggestionProvider<NuGetPackageSource>))]
-        public string PackageSource { get; set; }
+        [SuggestableValue(typeof(NuGetPackageSourceSuggestionProvider))]
+        public string? PackageSource { get; set; }
 
         [Category("Connection/Identity")]
         [ScriptAlias("FeedUrl")]
         [ScriptAlias("Url")]
         [DisplayName("NuGet server URL")]
         [PlaceholderText("Use server URL from package source")]
-        public string ApiEndpointUrl { get; set; }
+        public string? ApiEndpointUrl { get; set; }
         [Category("Connection/Identity")]
         [ScriptAlias("UserName")]
         [DisplayName("User name")]
         [PlaceholderText("Use user name from package source")]
-        public string UserName { get; set; }
+        public string? UserName { get; set; }
         [Category("Connection/Identity")]
         [ScriptAlias("Password")]
         [DisplayName("Password")]
         [PlaceholderText("Use password from package source")]
-        public string Password { get; set; }
+        public string? Password { get; set; }
         [Category("Connection/Identity")]
         [ScriptAlias("ApiKey")]
         [DisplayName("NuGet API Key")]
         [PlaceholderText("Use API Key from package source")]
-        public string ApiKey { get; set; }
+        public string? ApiKey { get; set; }
 
         public override async Task ExecuteAsync(IOperationExecutionContext context)
         {
-            var packagePath = context.ResolvePath(this.PackagePath);
+            var packagePath = context.ResolvePath(this.PackagePath!);
             if (string.IsNullOrEmpty(packagePath))
             {
                 this.LogError("Missing required argument \"Package\".");
@@ -72,77 +73,54 @@ namespace Inedo.Extensions.DotNet.Operations.NuGet
             }
 
             if (!string.IsNullOrEmpty(this.PackageSource))
-            {
-                this.LogDebug($"Using package source: {this.PackageSource}");
-                
-                var packageSource = (NuGetPackageSource)SecureResource.Create(this.PackageSource, (IResourceResolutionContext)context);
-                if (packageSource == null)
-                {
-                    this.LogError($"Package source \"{this.PackageSource}\" not found, or is not a NuGetPackageSource.");
-                    return;
-                }
-                this.ApiEndpointUrl = packageSource.ApiEndpointUrl;
+                this.ResolvePackageSource(context);
 
-                if (!string.IsNullOrEmpty(packageSource.CredentialName))
-                {
-                    this.LogDebug($"Using credentials: {packageSource.CredentialName}...");
-                    var credentials = packageSource.GetCredentials((ICredentialResolutionContext)context);
-                    if (credentials is TokenCredentials tokenCredentials)
-                    {
-                        this.ApiKey = AH.Unprotect(tokenCredentials.Token);
-                    }
-                    else if (credentials is UsernamePasswordCredentials usernameCredentials)
-                    {
-                        this.UserName = usernameCredentials.UserName;
-                        this.Password = AH.Unprotect(usernameCredentials.Password);
-                    }
-                    else
-                    {
-                        this.LogError($"Package source \"{packageSource.CredentialName}\" not found, or is not a Token or UsernamePassword type.");
-                        return;
-                    }
-                }
-            }
-
-            if (this.ApiEndpointUrl == null)
+            if (string.IsNullOrEmpty(this.ApiEndpointUrl))
             {
-                this.LogError($"No Url was specified to push a package to; you must either set a package source or the argument.");
+                this.LogError("FeedUrl is required if a package source is not specified.");
                 return;
-            }
-            if (!string.IsNullOrEmpty(this.UserName))
-            {
-                if (string.IsNullOrEmpty(this.Password))
-                    this.LogWarning($"Username specified but password is blank.");
-                
-                if (string.IsNullOrEmpty(this.ApiKey))
-                    this.ApiKey = this.UserName + ":" + this.Password;
-                else
-                    this.LogWarning($"ApiKey is specified, so Username/Password will be ignored");
-
             }
 
             var nugetInfo = await this.GetNuGetInfoAsync(context);
+
+            this.GetSourceUrl(out var pushUrl, out var displayUrl);
 
             this.LogInformation($"Pushing package {packagePath}...");
 
             if (nugetInfo.IsNuGetExe)
             {
+                var apiKeyArg = string.Empty;
+                var apiKeyDisplayArg = string.Empty;
+                if (!string.IsNullOrEmpty(this.ApiKey))
+                {
+                    apiKeyArg = $"-ApiKey \"{this.ApiKey}\" ";
+                    apiKeyDisplayArg = "-ApiKey XXXXX ";
+                }
+
                 await this.ExecuteNuGetAsync(
                     context,
                     nugetInfo,
-                    $"push \"{packagePath}\" -ApiKey \"{this.ApiKey}\" -Source \"{this.ApiEndpointUrl}\" -NonInteractive",
+                    $"push \"{packagePath}\" {apiKeyArg}-Source \"{pushUrl}\" -NonInteractive",
                     null,
-                    $"push \"{packagePath}\" -ApiKey XXXXX -Source \"{this.ApiEndpointUrl}\" -NonInteractive"
+                    $"push \"{packagePath}\" {apiKeyDisplayArg}-Source \"{displayUrl}\" -NonInteractive"
                 );
             }
             else
             {
+                var apiKeyArg = string.Empty;
+                var apiKeyDisplayArg = string.Empty;
+                if (!string.IsNullOrEmpty(this.ApiKey))
+                {
+                    apiKeyArg = $"--api-key \"{this.ApiKey}\" ";
+                    apiKeyDisplayArg = "--api-key XXXXX ";
+                }
+
                 await this.ExecuteNuGetAsync(
                     context,
                     nugetInfo,
-                    $"nuget push \"{packagePath}\" --api-key \"{this.ApiKey}\" --source \"{this.ApiEndpointUrl}\"",
+                    $"nuget push \"{packagePath}\" {apiKeyArg}\"{this.ApiKey}\" --source \"{pushUrl}\"",
                     null,
-                    $"nuget push \"{packagePath}\" --api-key XXXXX --source \"{this.ApiEndpointUrl}\""
+                    $"nuget push \"{packagePath}\" {apiKeyDisplayArg}--source \"{displayUrl}\""
                 );
             }
         }
@@ -159,6 +137,97 @@ namespace Inedo.Extensions.DotNet.Operations.NuGet
                     new Hilite(config[nameof(this.PackageSource)])
                 )
             );
+        }
+
+        private void ResolvePackageSource(ICredentialResolutionContext context)
+        {
+            this.LogDebug($"Using package source: {this.PackageSource}");
+
+            string? apiUrl;
+            string? apiKey = null;
+            string? userName = null;
+            string? password = null;
+
+            var packageSource = new PackageSourceId(this.PackageSource!);
+
+            switch (packageSource.Format)
+            {
+                case PackageSourceIdFormat.SecureResource:
+                    if (!context.TryGetSecureResource(packageSource.GetResourceName(), out var resource) || resource is not NuGetPackageSource nps)
+                    {
+                        this.LogError($"Package source \"{this.PackageSource}\" not found or is not a NuGetPackageSource.");
+                        return;
+                    }
+
+                    apiUrl = nps.ApiEndpointUrl;
+                    switch (nps.GetCredentials(context))
+                    {
+                        case TokenCredentials tc:
+                            apiKey = AH.Unprotect(tc.Token);
+                            break;
+
+                        case UsernamePasswordCredentials upc:
+                            if (upc.UserName == "api")
+                            {
+                                apiKey = AH.Unprotect(upc.Password);
+                            }
+                            else
+                            {
+                                userName = upc.UserName;
+                                password = AH.Unprotect(upc.Password);
+                            }
+                            break;
+
+                        case null:
+                            break;
+
+                        default:
+                            throw new ExecutionFailureException($"Invalid credential type: {nps.CredentialName}");
+                    }
+                    break;
+
+                case PackageSourceIdFormat.ProGetFeed:
+                    if (SecureCredentials.TryCreate(packageSource.GetProGetServiceCredentialName(), context) is not ProGetServiceCredentials creds)
+                        throw new ExecutionFailureException($"{this.PackageSource} is not valid.");
+
+                    apiKey = creds.APIKey;
+                    userName = creds.UserName;
+                    password = creds.Password;
+                    apiUrl = $"{creds.ServiceUrl!.TrimEnd('/')}/nuget/{Uri.EscapeDataString(packageSource.GetFeedName())}";
+                    break;
+
+                case PackageSourceIdFormat.Url:
+                    apiUrl = packageSource.GetUrl();
+                    break;
+
+                default:
+                    throw new NotSupportedException();
+            }
+
+            this.ApiEndpointUrl = AH.CoalesceString(this.ApiEndpointUrl, apiUrl);
+            this.ApiKey = AH.CoalesceString(this.ApiKey, apiKey);
+            this.UserName = AH.CoalesceString(this.UserName, userName);
+            this.Password = AH.CoalesceString(this.Password, password);
+        }
+        private void GetSourceUrl(out string url, out string displayUrl)
+        {
+            if (string.IsNullOrEmpty(this.UserName))
+            {
+                url = this.ApiEndpointUrl!;
+                displayUrl = url;
+            }
+            else
+            {
+                var builder = new UriBuilder(this.ApiEndpointUrl!)
+                {
+                    UserName = this.UserName,
+                    Password = this.Password ?? string.Empty
+                };
+
+                url = builder.ToString();
+                builder.Password = "XXXXX";
+                displayUrl = builder.ToString();
+            }
         }
     }
 }
