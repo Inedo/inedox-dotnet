0386d07a246ddcf412b8ce65ef1ed6ff8b25a626
Greg Divis <gdivis@inedo.com>
2023-04-03 09:34:09 -0400

    Possible errors when building projects with a space in the path #11

--- DotNet/InedoExtension/InternalExtensions.cs
+++ DotNet/InedoExtension/InternalExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 
 namespace Inedo.Extensions.DotNet
 {
@@ -12,40 +11,16 @@ namespace Inedo.Extensions.DotNet
         /// <param name="arg">The argument to append and wrap with quotes.</param>
         public static void AppendArgument(this StringBuilder sb, string arg)
         {
-            if (sb == null)
-                throw new ArgumentNullException(nameof(sb));
+            ArgumentNullException.ThrowIfNull(sb);
 
-            bool needsQuotes = false;
-            if (!string.IsNullOrEmpty(arg))
-            {
-                foreach (char c in arg)
-                {
-                    if (char.IsWhiteSpace(c) || c == '\"')
-                    {
-                        needsQuotes = true;
-                        break;
-                    }
-                }
-            }
+            if (string.IsNullOrEmpty(arg))
+                return;
 
-            if (needsQuotes)
-            {
-                sb.Append('\"');
-                sb.Append(arg);
-                // escape any quote characters after the first one
-                sb.Replace("\"", "\\\"", 1, sb.Length - 1);
-
-                // if last character is a \, add another one to prevent it from escaping the closing quote
-                if (sb[sb.Length - 1] == '\\')
-                    sb.Append('\\');
-
-                sb.Append('\"');
-            }
-            else
-            {
-                sb.Append(arg);
-            }
-
+            sb.Append('\"');
+            sb.Append(arg);
+            if (arg.EndsWith('\\'))
+                sb.Append('\\');
+            sb.Append('\"');
             sb.Append(' ');
         }
     }
